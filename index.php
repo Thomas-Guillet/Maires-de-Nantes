fbe721eef11782c017bcccb49be4566695ed2793
Unknown <thomas.guillet35@hotmail.fr>
2017-10-26 12:02:07 +0200

    move to php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+include_once "connect.php";
+include_once "dao/userDao.php";
+include_once "dao/articleDao.php";
+include_once "controller/setup_index.php";
+?>
+<html>
+	<head>
+		<title>Archives municipales de Nantes</title>
+	</head>
+	<body>
+		<div id="maire-1">
+			<div class="name">
+				Geoffroy DROUET,
+			</div>
+			<div class="label">
+			 sieur de Langle puis de Portric
+			</div>
+			<div class="begin-date">
+				1565
+			</div>
+		</div>
+	</body>
+</html>
